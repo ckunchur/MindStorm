93a06c867493102ca2f90ae8cb000753f6d6b408
Caitlin Kunchur <crkunchur@gmail.com>
2024-02-12 20:36:43 -0800

    landing and choose buddy

--- mindstorm-app/App.js
+++ mindstorm-app/App.js
@@ -1,14 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, ImageBackground, Dimensions } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useState, useEffect } from "react";
+import LandingScreen from './components/LandingScreen';
+import { Ionicons } from '@expo/vector-icons';
+import ChooseYourBuddy from './components/ChooseBuddy';
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let contentDisplayed; // set equal to list component if true
+  const [pressed, setPressed] = useState(false)
+
+  if (pressed) {
+    contentDisplayed = (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarLabelStyle: {
+            labelStyle: { fontSize: 14 }
+          },
+          tabBarIcon: ({ focused }) => {
+            let iconName;
+
+            if (route.name === 'Journal') {
+              iconName = focused ? 'pencil' : 'pencil-outline';
+            } else if (route.name === 'Insights') {
+              iconName = focused ? 'analytics' : 'analytics-outline';
+            } 
+            return <Ionicons name={iconName} size={24} color="black" />;
+          }
+        })}>
+        <Tab.Screen name="Journal" options={{headerShown: false}}  component={ChooseYourBuddy} />
+        <Tab.Screen name="Insights" options={{headerShown: false}} component={ChooseYourBuddy} />
+      </Tab.Navigator>
+     
+  </NavigationContainer>
+    )
+
+  }
+  else { 
+    contentDisplayed = (
+    <SafeAreaView style={styles.container}> 
+      <LandingScreen setPressed={setPressed}/>
+    </SafeAreaView> 
+    )
+  }
+
+  return contentDisplayed;
 }
+
 
 const styles = StyleSheet.create({
   container: {
