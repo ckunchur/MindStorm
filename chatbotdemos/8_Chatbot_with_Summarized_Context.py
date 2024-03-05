96c818b67b82fc12e848a8e9343f5c56561dc0af
Caitlin Kunchur <crkunchur@gmail.com>
2024-03-05 15:01:26 -0800

    cleaned code structure

--- chatbotdemos/8_Chatbot_with_Summarized_Context.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.write("Chatbot where past entries are summarized and appended to the in convo chat history.")
--- chatbotdemos/9_Chatbot_with_RAG.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.write("Chatbot with RAG on past entries")
