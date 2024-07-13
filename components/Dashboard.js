b7679f3c4dfc6a92f5a6dfdfb7962aebdf779345
sonyashijin <sonyajin@stanford.edu>
2024-07-12 19:23:52 -0700

    dashboard update

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { Button, Card, ProgressBar, Text, Avatar } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import {ChatScreen} from './ChatScreen'; // Import the ChatScreen component
+import profileImage from '../assets/profilepic.png'; // Adjust the path to your profile image
 
 const Dashboard = ({ navigation }) => {
   const [userName, setUserName] = useState("John Doe");
@@ -17,8 +20,17 @@ const Dashboard = ({ navigation }) => {
     <Card key={index} style={styles.goalCard}>
       <Card.Title title={goal.title} />
       <Card.Content>
-        <ProgressBar progress={goal.progress} style={styles.progressBar} />
-        <Button mode="contained" style={styles.updateButton}>Start goal chat</Button>
+        <ProgressBar progress={goal.progress} style={styles.progressBar} color="#000" />
+        <LinearGradient
+          colors={['#78bbcb', '#5c8bb2']}
+          start={[0, 0]}
+          end={[1, 0]}
+          style={styles.updateButtonGradient}
+        >
+          <Button mode="contained" style={styles.updateButton}>
+            <Text style={styles.updateButtonText}>Start goal chat</Text>
+          </Button>
+        </LinearGradient>
       </Card.Content>
     </Card>
   );
@@ -30,21 +42,33 @@ const Dashboard = ({ navigation }) => {
           <Text style={styles.greetingText}>Hello, {userName}</Text>
           <Text style={styles.subGreetingText}>Welcome back!</Text>
         </View>
-        <Avatar.Image size={48} source={{ uri: profilePicture }} style={styles.profilePicture} />
+        <Avatar.Image size={48} source={profileImage} style={styles.profilePicture} />
       </View>
       <View style={styles.checkInContainer}>
         <View style={styles.checkInTextContainer}>
-          {/* <Ionicons name="chatbubble-ellipses-outline" size={20} color="#000" /> */}
           <View style={styles.checkInTextWrapper}>
             <Text style={styles.checkInText}>Ready to check in? 🙃</Text>
             <Text style={styles.checkInSubText}>Let's hear how your day is going so far.</Text>
           </View>
         </View>
-        <Button mode="contained" style={styles.arrowButton} onPress={() => navigation.navigate('ChatScreen')}>
-          <Ionicons name="arrow-forward" size={24} color="#fff" />
-        </Button>
+        <LinearGradient
+          colors={['#78bbcb', '#5c8bb2']}
+          start={[0, 0]}
+          end={[1, 0]}
+          style={styles.arrowButtonGradient}
+        >
+          <Button mode="contained" style={styles.arrowButton} onPress={() => navigation.navigate('ChatScreen')}>
+            <Ionicons name="arrow-forward" size={24} color="white" />
+          </Button>
+        </LinearGradient>
       </View>
-      <Text style={styles.sectionTitle}>Current Goals</Text>
+      <View style={styles.goalsHeaderContainer}>
+        <Text style={styles.sectionTitle}>Current Goals</Text>
+        <TouchableOpacity>
+          <Text style={styles.editLink}>edit</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.goalsSubheader}>Want to check in on a specific goal? Start a goal-oriented chat.</Text>
       <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.goalsContainer}>
         {goals.map(renderGoal)}
       </ScrollView>
@@ -59,7 +83,7 @@ const Dashboard = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     padding: 16,
-    paddingTop: 100, // Increased top padding
+    marginTop: 150,
     marginLeft: 20,
     marginRight: 20,
   },
@@ -67,32 +91,32 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 20, // Increased bottom margin
+    marginBottom: 20,
   },
   greetingText: {
-    fontSize: 28, // Increased font size
+    fontSize: 28,
     fontWeight: 'bold',
-    marginBottom: 4, // Added margin between greeting lines
+    marginBottom: 4,
   },
   subGreetingText: {
-    fontSize: 18, // Increased font size
+    fontSize: 18,
     color: '#666',
   },
   profilePicture: {
-    marginLeft: 50, // Increased left margin
+    marginLeft: 50,
     marginRight: 10,
   },
   checkInContainer: {
-    marginTop: 10, // Increased top margin
+    marginTop: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-    paddingVertical: 20, // Increased vertical padding
-    paddingHorizontal: 10, // Increased horizontal padding
+    backgroundColor: '#fefefe',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
     paddingLeft: 20,
-    borderRadius: 12, // Increased border radius
-    marginBottom: 24, // Increased bottom margin
+    borderRadius: 12,
+    marginBottom: 24,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
@@ -102,69 +126,113 @@ const styles = StyleSheet.create({
   checkInTextContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    flex: 1, // Added flex to allow text to wrap
+    flex: 1,
   },
   checkInTextWrapper: {
-    marginLeft: 0, // Increased left margin
-    flex: 1, // Added flex to allow text to wrap
+    marginLeft: 0,
+    flex: 1,
   },
   checkInText: {
-    fontSize: 18, // Increased font size
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 8, // Added margin between text lines
+    marginBottom: 8,
   },
   checkInSubText: {
-    fontSize: 16, // Increased font size
+    fontSize: 16,
     color: '#666',
   },
+  arrowButtonGradient: {
+    borderRadius: 24,
+    padding: 5,
+    marginRight: 20, // Added right margin to create space from the edge
+  },
   arrowButton: {
-    backgroundColor: '#000',
-    borderRadius: 24, // Increased border radius
-    padding: 5, // Increased padding
-    marginLeft: 16, // Added left margin to separate from text
-    marginRight: 10,
+    backgroundColor: 'transparent',
+    borderRadius: 24,
+    padding: 5,
+  },
+  goalsHeaderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 24,
+    marginBottom: 8,
   },
   sectionTitle: {
-    marginTop: 24, // Increased top margin
-    fontSize: 24, // Increased font size
+    fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 16, // Decreased bottom margin
+  },
+  editLink: {
+    fontSize: 16,
+    color: '#666',
+    textDecorationLine: 'underline',
+  },
+  goalsSubheader: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 16,
   },
   goalsContainer: {
     flexDirection: 'row',
-    marginBottom: 24, // Increased bottom margin
+    marginBottom: 24,
   },
   goalCard: {
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#fefefe',
     marginRight: 16,
     marginLeft: 5,
     width: 250,
-    height: 180, // Adjusted height
-    borderRadius: 12, // Increased border radius
-    marginVertical: 8, // Added vertical margin to prevent cut-off
+    height: 180,
+    borderRadius: 12,
+    marginVertical: 8,
+    padding: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
   },
   progressBar: {
-    marginTop: 12, // Increased top margin
-    marginBottom: 40, // Increased bottom margin
+    marginTop: 12,
+    marginBottom: 20, // Added bottom margin to create space
+    backgroundColor: '#E0E0E0',
+  },
+  updateButtonGradient: {
+    borderRadius: 24,
+    padding: 5,
+    marginTop: 10, // Added top margin to create space from the progress bar
   },
   updateButton: {
+    backgroundColor: 'transparent',
     alignSelf: 'flex-start',
   },
+  updateButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
   coachMessageContainer: {
-    marginTop: 0,
-    padding: 5, // Increased padding
-    backgroundColor: '#f4f4f4',
-    borderRadius: 12, // Increased border radius
+    marginTop: 10,
+    backgroundColor: '#fefefe',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 12,
+    marginBottom: 24,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
   },
   coachMessageTitle: {
-    fontSize: 20, // Increased font size
+    fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 12, // Increased bottom margin
+    marginBottom: 12,
   },
   coachMessage: {
-    marginTop: 0,
-    fontSize: 18, // Increased font size
-    lineHeight: 24, // Added line height for better readability
+    marginTop: 10,
+    fontSize: 18,
+    lineHeight: 27,
   },
 });
 
