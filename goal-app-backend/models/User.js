45ecdb25bdb4dbbfedfc2b897add3e3a2e4e434c
sonyashijin <sonyajin@stanford.edu>
2024-07-13 03:05:00 -0700

    backend update, cleaning repo

--- goal-app-backend/models/User.js
+++ goal-app-backend/models/User.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+  }
+  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
